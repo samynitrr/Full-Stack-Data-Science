754da0cfd4004240f14347f0447183fe672cf6de
samynitrr <sameershekhar7@gmail.com>
2022-03-11 14:12:14 +0530

    add files via upload

---
+++ SQL/Assignment/Assignment1.sql
@@ -0,0 +1,17 @@
+#Q1. Write an SQL query to fetch "FIRST_NAME" from Worker table using the alias name as <WORKER_NAME>.
+
+select FIRST_NAME as WORKER_NAME from Worker;
+
+
+
+
+#Q2. Write and SQL query to fetch unique values of DEPARTMENTT from Worker table.
+
+select distinct(DEPARTMENT) from Worker;
+
+
+
+
+#Q3. Write an SQL query to show the last 5 record from a table
+
+select * from table1 order by id desc limit 5;
